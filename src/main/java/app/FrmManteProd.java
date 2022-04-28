0194ab2270a68b9f7eda4b903ec2998c2abc14e6
Adrianzzziny <adri-escobar@hotmail.com>
2022-04-28 04:03:58 -0500

    3er commit CL1

--- src/main/java/app/FrmManteProd.java
+++ src/main/java/app/FrmManteProd.java
@@ -31,6 +31,9 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import com.mysql.cj.protocol.Resultset;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class FrmManteProd extends JFrame {
 
@@ -40,7 +43,8 @@ public class FrmManteProd extends JFrame {
 	private JTextField txtCantidad;
 	private JTextField txtPrecio;
 	private JTextField txtOrigen;
-	private JTable tablaRegistro;
+	private JTable tablaLibros;
+	private JScrollPane scrollPane;
 	/**
 	 * Launch the application.
 	 */
@@ -70,73 +74,146 @@ public class FrmManteProd extends JFrame {
 		contentPane.setLayout(null);
 
 		JButton btnNewButton = new JButton("Registrar");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				registrar();
 			}
 		});
-		btnNewButton.setBounds(23, 172, 89, 23);
+		btnNewButton.setBounds(23, 199, 144, 23);
 		contentPane.add(btnNewButton);
 		
 		cboTemas = new JComboBox();
-		cboTemas.setBounds(122, 129, 110, 22);
+		cboTemas.setFont(new Font("Tahoma", Font.BOLD, 11));
+		cboTemas.setBounds(122, 146, 144, 22);
 		contentPane.add(cboTemas);
 		
 		JLabel lblTema = new JLabel("Tema");
-		lblTema.setBounds(10, 133, 102, 14);
+		lblTema.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblTema.setBounds(10, 150, 102, 14);
 		contentPane.add(lblTema);
 		
 		JLabel lblTitulo = new JLabel("Titulo");
-		lblTitulo.setBounds(10, 11, 102, 14);
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblTitulo.setBounds(10, 21, 102, 14);
 		contentPane.add(lblTitulo);
 		
 		txtTitulo = new JTextField();
+		txtTitulo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int key = e.getKeyChar();
+
+			    boolean mayusculas = key >= 65 && key <= 90;
+			    boolean minusculas = key >= 97 && key <= 122;
+			    boolean espacio = key == 32;
+			            
+			     if (!(minusculas || mayusculas || espacio))
+			    {
+			        e.consume();
+			    }
+			}
+		});
 		txtTitulo.setColumns(10);
-		txtTitulo.setBounds(122, 8, 144, 20);
+		txtTitulo.setBounds(122, 18, 144, 20);
 		contentPane.add(txtTitulo);
 		
 		JLabel lblCantidad = new JLabel("Cantidad:");
-		lblCantidad.setBounds(10, 70, 102, 14);
+		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCantidad.setBounds(10, 87, 102, 14);
 		contentPane.add(lblCantidad);
 		
 		txtCantidad = new JTextField();
+		txtCantidad.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int key = e.getKeyChar();
+
+			    boolean numeros = key >= 48 && key <= 57;
+			        
+			    if (!numeros)
+			    {
+			        e.consume();
+			    }
+
+			    if (txtCantidad.getText().trim().length() == 10) {
+			        e.consume();
+			    }
+			}
+		});
 		txtCantidad.setColumns(10);
-		txtCantidad.setBounds(122, 67, 110, 20);
+		txtCantidad.setBounds(122, 84, 110, 20);
 		contentPane.add(txtCantidad);
 		
 		JLabel lblPrecio = new JLabel("Precio:");
-		lblPrecio.setBounds(10, 42, 102, 14);
+		lblPrecio.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblPrecio.setBounds(10, 56, 102, 14);
 		contentPane.add(lblPrecio);
 		
 		txtPrecio = new JTextField();
+		txtPrecio.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int key = e.getKeyChar();
+
+			    boolean numeros = key >= 48 && key <= 57;
+			        
+			    if (!numeros)
+			    {
+			        e.consume();
+			    }
+
+			    if (txtPrecio.getText().trim().length() == 10) {
+			        e.consume();
+			    }
+			}
+		});
 		txtPrecio.setColumns(10);
-		txtPrecio.setBounds(122, 39, 110, 20);
+		txtPrecio.setBounds(122, 53, 110, 20);
 		contentPane.add(txtPrecio);
 		
 		JLabel lblOrigen = new JLabel("Origen");
-		lblOrigen.setBounds(10, 97, 46, 14);
+		lblOrigen.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblOrigen.setBounds(10, 118, 46, 14);
 		contentPane.add(lblOrigen);
 		
 		txtOrigen = new JTextField();
-		txtOrigen.setBounds(122, 98, 110, 20);
+		txtOrigen.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int key = e.getKeyChar();
+
+			    boolean mayusculas = key >= 65 && key <= 90;
+			    boolean minusculas = key >= 97 && key <= 122;
+			    boolean espacio = key == 32;
+			            
+			     if (!(minusculas || mayusculas || espacio))
+			    {
+			        e.consume();
+			    }
+			}
+		});
+		txtOrigen.setBounds(122, 115, 110, 20);
 		contentPane.add(txtOrigen);
 		txtOrigen.setColumns(10);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 227, 414, 144);
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 242, 414, 191);
 		contentPane.add(scrollPane);
 		
-		tablaRegistro = new JTable();
-		tablaRegistro.setModel(new DefaultTableModel(
+		tablaLibros = new JTable();
+		tablaLibros.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
 				"ID", "Titulo", "Precio", "Cantidad", "Origen", "Tema"
 			}
 		));
-		scrollPane.setViewportView(tablaRegistro);
+		scrollPane.setViewportView(tablaLibros);
 		
+		listado();
 		llenaCombo();
+	
 	}
 
 	void llenaCombo() {
@@ -182,8 +259,26 @@ public class FrmManteProd extends JFrame {
 		
 		JOptionPane.showMessageDialog(this, "Producto Registrado");
 		
+		
 		}
+	
+	public void listado() {
+        EntityManagerFactory fabrica=Persistence.createEntityManagerFactory("mysql");
+        EntityManager em = fabrica.createEntityManager();
+
+        TypedQuery<Libro> consulta= em.createQuery("select pro from Libro pro", Libro.class);
+        List<Libro> listadoProductos = consulta.getResultList();
+
+         DefaultTableModel tablaRegista=(DefaultTableModel) tablaLibros.getModel();
+         tablaRegista.setRowCount(0);
+        for(Libro p: listadoProductos) {
+            Object row[]= {p.getIdlibro(),p.getTituloLibro(),p.getPrecio(),p.getCantEjemplares(),p.getOrigen(),p.getIdTema()};
+            tablaRegista.addRow(row);
+        }
+	}
+	
 }
+
 
 
 
